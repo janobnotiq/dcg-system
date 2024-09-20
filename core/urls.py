9595afb0ebec60cb63731a59c6020f83074d1ee3
janobnotiq <janobnotiq@gmail.com>
2024-09-20 14:58:26 +0500

    bug

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("app.urls")),
+    path("login/",auth_views.LoginView.as_view(template_name="login.html"),name="login")
 ]
 
 if settings.DEBUG:
