2dd12d4f72706006b56f7e0873ccb8e1f098b970
janobnotiq <janobnotiq@gmail.com>
2024-09-20 23:04:58 +0500

    one bug fixed, another one appeared

--- core/settings.py
+++ core/settings.py
@@ -121,5 +121,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL="/home/"
-APPEND_SLASH=False
+LOGIN_REDIRECT_URL="home"
--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("app.urls")),
-    path("login/",auth_views.LoginView.as_view(template_name="login.html"),name="login")
+    path("login/",auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/",auth_views.LogoutView.as_view(), name="logout"),
 ]
 
 if settings.DEBUG:
